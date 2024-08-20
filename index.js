8ae77b2ef7996dd4963db071d269dae1d556bc44
AmineGithubIOS <ofabrice15@outlook.com>
2024-08-20 23:20:40 +0000

    Mise en place du code de démarrage

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const { Client } = require("discord.js")
 
+const client = new Client({intents: 53608447})
+client.on("ready", () => {
+    console.log('✅ Le bot est en ligne')
+})
+
+client.login("MTI3MzA0MzkzNDk1MTY0MTI0NA.G3f_A4.t1dO4CWeHS2yZ0VIcuDbhy5Yh72vWk0P-6PRG8")
