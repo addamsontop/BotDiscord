bce8b9d41f7cd96e6021b50b80ba8b0a065085df
Addams <ofabrice15@outlook.com>
2024-08-21 08:27:02 +0000

    Mise a jour

--- index.js
+++ index.js
@@ -5,16 +5,4 @@ client.on('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
-    if (message.author.bot) return;
-    if (message.content.toLowerCase() === 'bonjour bot') {
-        message.channel.send('Bonjour à toi aussi!');
-    }
-
-    if (message.content.toLowerCase() === 'comment ça va ?') {
-        message.channel.send('Je vais bien, merci de demander !');
-    }
-});
-
-
 client.login("MTI3MzA0MzkzNDk1MTY0MTI0NA.G3f_A4.t1dO4CWeHS2yZ0VIcuDbhy5Yh72vWk0P-6PRG8")
