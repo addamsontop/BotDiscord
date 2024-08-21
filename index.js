9a2365c2cf455d7b8ef577ece3f99ffd5262748e
Addams <ofabrice15@outlook.com>
2024-08-21 11:20:49 +0000

    Correction de bug et rajout d'un handler de commands slash

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 const config = require("./config.json")
+const loadCommands = require("./Handler/CommandHandler")
 
 client.on('ready', () => {
     console.log(`✅ Le bot est désormais en ligne`);
-    require("./SaveCommands/SaveCommand.js")
 });
+
+loadCommands(client)
 
 client.login(config.token)
