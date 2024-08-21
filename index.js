ff0f862b3836985695175dbcdc9d39395486fdb2
Addams <ofabrice15@outlook.com>
2024-08-21 11:11:13 +0000

    MAJ 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-const config = require("config.json")
+const config = require("./config.json")
 
 client.on('ready', () => {
     console.log(`✅ Le bot est désormais en ligne`);
